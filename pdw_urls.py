52d49c14122c5c09cb85d857395ff40e91362339
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2013-09-20 14:55:21 -0700

    Allow pdw_urls.py to spew item urls and potentially missing pokemon forms.

--- pdw_urls.py
+++ pdw_urls.py
@@ -5,6 +5,9 @@ URL = "http://cdn2.pokemon-gl.com/src/swf/theme/assets/global/parts/pokemon/scal
 NUTS_PATH = "http://cdn2.pokemon-gl.com/src/swf/theme/assets/global/parts/nuts/90/{0}.swf"
 ITEM_PATH = "http://cdn2.pokemon-gl.com/src/swf/theme/assets/global/parts/item/90/{0}.swf"
 
+MAXPOKEMON = 649
+MAXITEM = 638
+
 # Item sizes: 28, 90
 # Nuts sizes: 28, 35, 57, 90, 
 
@@ -53,7 +56,7 @@ def geturl(number, form=0):
 #     id - 148
 
 def getitem(item_id):
-    if 149 <= item_id <= 213:
+    if 149 <= item_id < 213:
         return NUTS_PATH.format(encrypt_kinomi(item_id - 148))
     else:
         return ITEM_PATH.format(encrypt_kinomi(item_id))
@@ -66,24 +69,48 @@ def getitem(item_id):
 #    print(geturl(386, form))
 
 import sys
-if len(sys.argv) == 1:
-    for number in range(1, 649 + 1):
-        forms = FORMS.get(number, [''])
-        for form_index, form_name in enumerate(forms):
-            if form_name:
-                name = "{}-{}.swf".format(number, form_name)
-            else:
-                name = "{}.swf".format(number)
-            print(geturl(number, form_index), name)
-elif len(sys.argv) == 2:
-    if sys.argv[1] == 'missing':
-        for number in range(1, 649+1):
-            form_index = len(FORMS.get(number, ['']))
-            print(geturl(number, form_index))
-    else:
-        print(geturl(int(sys.argv[1]), 0))
-elif len(sys.argv) == 3:
-    print(geturl(int(sys.argv[1]), int(sys.argv[2])))
+if len(sys.argv) <= 1:
+    mode, args = 'pokemon', []
 else:
-    print("Usage: pdw_urls.py [number [form]]", file=sys.stderr)
+    if sys.argv[1].isdigit():
+        mode, args = 'pokemon', sys.argv[1:]
+    else:
+        mode, args = sys.argv[1], sys.argv[2:]
 
+def usage():
+    print("Usage: pdw_urls.py [pokemon] [NUMBER [FORM]]", file=sys.stderr)
+    print("Usage: pdw_urls.py missing", file=sys.stderr)
+    print("Usage: pdw_urls.py item [NUMBER]", file=sys.stderr)
+    #print("Usage: pdw_urls.py nuts [NUMBER]", file=sys.stderr)
+
+if mode == 'pokemon':
+    if len(args) == 0:
+        for number in range(1, MAXPOKEMON + 1):
+            forms = FORMS.get(number, [''])
+            for form_index, form_name in enumerate(forms):
+                if form_name:
+                    name = "{}-{}.swf".format(number, form_name)
+                else:
+                    name = "{}.swf".format(number)
+                print(geturl(number, form_index), name)
+    elif len(args) == 1:
+        print(geturl(int(args[0]), 0))
+    elif len(args) == 2:
+        print(geturl(int(sys.argv[1]), int(sys.argv[2])))
+    else:
+        usage()
+elif mode == 'missing':
+    for number in range(1, MAXPOKEMON + 1):
+        form_index = len(FORMS.get(number, ['']))
+        print(geturl(number, form_index))
+elif mode == 'item':
+    if len(args) == 0:
+        for number in range(1, MAXITEM + 1):
+            name = "{}.swf".format(number)
+            print(getitem(number), name)
+    elif len(args) == 1:
+        print(getitem(int(args[0])))
+    else:
+        usage()
+else:
+    usage()
